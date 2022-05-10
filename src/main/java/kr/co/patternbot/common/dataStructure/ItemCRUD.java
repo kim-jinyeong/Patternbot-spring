c77ab8bec72bafce27055d0d0894cbedf2054840
coder-jinyeong <78059103+coder-jinyeong@users.noreply.github.com>
2022-05-10 20:15:57 +0900

    Builder

---
+++ src/main/java/kr/co/patternbot/common/dataStructure/ItemCRUD.java
@@ -0,0 +1,18 @@
+/**
+* packageName: kr.co.patternbot.common.dataStructure
+* fileNa     : itemCRUD
+* au         : kimjinyeong
+* date       : 2022-05-10
+* desc       : 
+* class variable : 
+*instance variable : 
+* area variable : 
+* parameter :     
+* ================================
+*  DATE              AUTHOR        NOTE
+* ================================
+*  2022-05-10         kimjinyeong    최초 생성        
+*/
+
+package kr.co.patternbot.common.dataStructure;public class itemCRUD {
+}
