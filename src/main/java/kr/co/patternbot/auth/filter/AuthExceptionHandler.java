33664246b9a68865734178b63ccf9595423b0969
coder-jinyeong <78059103+coder-jinyeong@users.noreply.github.com>
2022-05-25 12:21:45 +0900

    proxy

---
+++ src/main/java/kr/co/patternbot/auth/filter/AuthExceptionHandler.java
@@ -0,0 +1,104 @@
+/**
+ * packageName: kr.co.patternbot.auth.filter
+ * fileNa     : AuthExceptionHandler
+ * au         : kimjinyeong
+ * date       : 2022-05-25
+ * desc       :
+ * class variable :
+ * instance variable :
+ * area variable :
+ * parameter :
+ * ================================
+ * DATE              AUTHOR        NOTE
+ * ================================
+ * 2022-05-25         kimjinyeong    최초 생성
+ */
+
+package kr.co.patternbot.auth.filter;
+
+import kr.co.patternbot.auth.domains.Messenger;
+import kr.co.patternbot.auth.exception.ErrorCode;
+import kr.co.patternbot.auth.exception.LoginRunnerException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.nio.file.AccessDeniedException;
+
+@Slf4j
+@ControllerAdvice
+public class AuthExceptionHandler {
+    @ExceptionHandler(RuntimeException.class)
+    protected ResponseEntity<Messenger> handleRuntimeException(RuntimeException e){
+        log.info("handleRuntimeException", e);
+        Messenger response = Messenger.builder().code("test")
+                .message(e. getMessage())
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .build();
+        return new ResponseEntity<>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+
+    }
+    @ExceptionHandler(SecurityException.class)
+    protected ResponseEntity<Messenger> handleSecurityException(SecurityException e){
+        log.info("handleSecurityException",e);
+
+        Messenger response = Messenger.builder()
+                .code(ErrorCode.AUTHENTICATION_FAILED.getCode())
+                .message(e.getMessage())
+                .status(ErrorCode.AUTHENTICATION_FAILED.getStatus())
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(LoginRunnerException.class)
+    protected ResponseEntity<Messenger> handleLoginRunnerException(LoginRunnerException e){
+        log.info("handleLoginRunnerException",e);
+
+        Messenger response = Messenger.builder()
+                .code(ErrorCode.LOGIN_FAILED.getCode())
+                .message(ErrorCode.LOGIN_FAILED.getMsg())
+                .status(ErrorCode.LOGIN_FAILED.getStatus())
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    protected ResponseEntity<Messenger> handleBadCredentialsException(BadCredentialsException e){
+        log.info("handleBadCredentialsException",e);
+
+        Messenger response = Messenger.builder()
+                .code(ErrorCode.ACCESS_DENIED.getCode())
+                .message(ErrorCode.ACCESS_DENIED.getMsg())
+                .status(ErrorCode.ACCESS_DENIED.getStatus())
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    protected ResponseEntity<Messenger> handleAccessDeniedException(AccessDeniedException e){
+        log.info("handleAccessDeniedException",e);
+
+        Messenger response = Messenger.builder()
+                .code(ErrorCode.ACCESS_DENIED.getCode())
+                .message(ErrorCode.ACCESS_DENIED.getMsg())
+                .status(ErrorCode.ACCESS_DENIED.getStatus())
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(InsufficientAuthenticationException.class)
+    protected ResponseEntity<Messenger> handleInsufficientAuthenticationException(InsufficientAuthenticationException e){
+        log.info("handleAInsufficientAuthenticationException",e);
+
+        Messenger response = Messenger.builder()
+                .code(ErrorCode.AUTHENTICATION_FAILED.getCode())
+                .message(ErrorCode.AUTHENTICATION_FAILED.getMsg())
+                .status(ErrorCode.AUTHENTICATION_FAILED.getStatus())
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
+    }
+}
